c2b31ea336c09925c8ec1f7686b9e08614af5e38
SunshineFF <780342040@qq.com>
2019-05-24 18:10:29 +0800

    增加关系表，阻止系统更新用户等级

--- Application/Mobile/Controller/DisController.class.php
+++ Application/Mobile/Controller/DisController.class.php
@@ -2,6 +2,7 @@
 
 namespace Mobile\Controller;
 
+use Home\Logic\UsersLogic;
 class DisController extends MobileBaseController
 {
     protected $user_id;
@@ -30,6 +31,68 @@ class DisController extends MobileBaseController
     }
 
     public function index(){
+        $data = $this->initDataFromUser($this->user);
+        $this->assign('data',json_encode($data));
         return $this->display();
     }
+
+    protected function initDataFromUser($user){
+        if (!$user['parent_path']){
+            return [];
+        }
+        $firstParentId = current(explode(',',$user['parent_path']));
+        $userModel = M('users');
+        $firstParent = $userModel->where(['user_id' => $firstParentId])->find();
+        $dataNew = [];
+        $allChild = unserialize($firstParent['all_child']);
+        $current = $user['user_id'];
+        $currentLevel = [];
+        for ($i = 0;$i < count($allChild) ; $i++){
+            if (!isset($allChild[$current])){
+                if (count($currentLevel) == 0)break;
+                $key = array_keys($currentLevel)[0];
+                $current = $currentLevel[$key];
+                unset($currentLevel[$key]);
+                continue;
+            }
+            $one = $allChild[$current];
+            if (isset($one[2])){
+                $dataNew[] = $one[2];
+                $dataNew[] = $one[1];
+                $currentLevel[] = $one[1];
+                $currentLevel[] = $one[2];
+            }else{
+                $dataNew[] = $one[1];
+                $currentLevel[] = $one[1];
+            }
+            if (count($currentLevel) > 0){
+                $key = array_keys($currentLevel)[0];
+                $current = $currentLevel[$key];
+                unset($currentLevel[$key]);
+            }
+        }
+        if (count($dataNew) == 0){
+            return [];
+        }
+        $data = M('users')->query("select user_id id,parent_id pId,mobile name from ty_users where user_id in (".array_to_string($dataNew).") order by user_id desc");
+        $one = ['id'=>$user['user_id'],'pid'=>0,'name' => $user['mobile'],'isParent'=>true];
+        $data[] = $one;
+        arsort($data);
+        $dataNew = [];
+        foreach ($data as $one){
+            $one['pId'] = $one['pid'];
+            unset($one['pid']);
+            $dataNew[] = $one;
+        }
+        return $dataNew;
+    }
+
+}
+
+function array_to_string($array){
+    $string = '';
+    $string .= "'";
+    $string .= implode("','",$array);
+    $string .= "'";
+    return $string;
 }
